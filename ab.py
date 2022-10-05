9319433b1949355f0e5d840418a76730682850e8
randompast <matteroflight@gmail.com>
2022-10-05 15:53:48 -0400

    added performance analysis

--- ab.py
+++ ab.py
@@ -1,10 +1,12 @@
 import random
 import pygame
+import time
 
 class mazegen():
     def __init__(self, size):
         self.size = size
         self.nvisited = 0
+        self.steps = 0
         self.visited = self.make_matrix(size, size, 0)
         self.vwalls = self.make_matrix(size+1, size+1, 1)
         self.hwalls = self.make_matrix(size+1, size+1, 1)
@@ -28,14 +30,8 @@ class mazegen():
                     pygame.draw.rect(surf, (255,255,255), [i*scale + 50, j*scale + 50, scale/10, scale], scale//4)
 
     def AldousBroder(self):
-        p = self.current_point
-
         while(self.nvisited < self.size**2):
-            next_point = self.pickNeighbor(p)
-            if ( self.visited[next_point[0]][next_point[1]] == 0 ):
-                self.visit(next_point)
-                self.removeWall(p, next_point)
-            p = next_point
+            self.AldousBroder_step()
 
     def AldousBroder_step(self):
         if (self.nvisited < self.size**2):
@@ -44,6 +40,8 @@ class mazegen():
                 self.visit(next_point)
                 self.removeWall(self.current_point, next_point)
             self.current_point = next_point
+            self.steps += 1
+
 
     def removeWall(self, p1, p2):
         dx = p2[0] - p1[0]
@@ -94,3 +92,50 @@ class mazegen():
         for i in range(n):
             arr += [[v]*m]
         return arr
+
+
+def avg_perf(s, n):
+    g = []
+    start_time = time.time()
+    for _ in range(n):
+        a = mazegen(s)
+        a.AldousBroder()
+        g += [a.steps]
+    return g, s, time.time() - start_time
+
+
+def performance_characteristics():
+    import concurrent.futures as conf
+
+    results = []
+    with conf.ProcessPoolExecutor() as executor:
+        for i in range(2, 15):
+            f = executor.submit(avg_perf, i, 10)
+            results.append(f)
+        results = [r.result() for r in results]
+
+    plot_performance(results)
+
+def plot_performance(r):
+    from matplotlib import pyplot as plt
+    for i in r:
+        steps = i[0]
+        size = i[1]
+        t = i[2]
+        plt.scatter( [size]*len(steps), steps )
+    x = range(2,r[-1][1]+1)
+    y = [ sum(i[0]) / len(i[0]) for i in r ]
+
+    plt.title('Aldous Broder Performance ~ O(n^3.1)')
+    plt.plot( x, [ i**3.4 for i in x], color='blue', label='s^3.4' )
+    plt.plot( x, [ i**3.1 for i in x], color='blue', label='s^3.1' )
+    plt.plot( x, [ i**2.7 for i in x], color='blue', label='s^2.7' )
+    plt.plot( x, y, color='green', label='avg' )
+    plt.xlabel("s x s grid")
+    plt.ylabel("# of steps to finish")
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+if __name__ == '__main__':
+    performance_characteristics()
--- gameloop.py
+++ gameloop.py
@@ -4,15 +4,17 @@ from pygame.locals import *
 from ab import mazegen
 
 def gameloop():
-    size, scale = 5,80
+    size, scale = 4,80
     r = size * scale + 100
     surf = pygame.display.set_mode((r,r))
     a = mazegen(size)
     while True:
         pygame.display.update()
         surf.fill((0,0,0))
-        a.AldousBroder_step()
+        a = mazegen(size)
+        a.AldousBroder()
+        # a.AldousBroder_step()
         a.instantiateWallMaze(surf, scale)
-        if a.nvisited == a.size**2: a = mazegen(size)
-        pygame.time.Clock().tick(40)
+        # if a.nvisited == a.size**2: a = mazegen(size)
+        pygame.time.Clock().tick(2)
 
